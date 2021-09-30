3ae8571ed14aac39b04693af555d65b75e0f1453
Ivo Pereira <ivopereira.jr12@gmail.com>
2021-09-30 19:44:11 -0300

    :sparkles: adicionado Header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import Routes from './routes'
+import Header from "./components/Header";
 
 import './styles/main.scss'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <ToastContainer autoClose={3000} />
     </BrowserRouter>
