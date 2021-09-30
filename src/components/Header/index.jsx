f253e0be1c655694cf8bd34e6f088960a86de4c0
Ivo Pereira <ivopereira.jr12@gmail.com>
2021-09-30 19:42:36 -0300

    :sparkles: Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import '../../styles/assistants.scss';
+import './styles.scss';
+
+export default function Header() {
+  return (
+    <header className="container">
+      <div className="content">
+        <Link to="/">
+          <img src="/assets/logo.png" alt="logo marca playstation store" />
+        </Link>
+
+        <Link to="cart" className="cart">
+          <div>
+            <strong>Meu carrinho</strong>
+            <span>0 item</span>
+          </div>
+          <MdAddShoppingCart size={36} color="#333" />
+        </Link>
+      </div>
+    </header>
+  );
+}
