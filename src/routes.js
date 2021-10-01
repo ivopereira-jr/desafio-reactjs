8f29b6b6ce49b5e3879c2fd5f942251471f232fa
Ivo Pereira <ivopereira.jr12@gmail.com>
2021-10-01 16:28:43 -0300

    :sparkles: rota cart

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Switch, Route } from "react-router";
 
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/cart" component={Cart} />
     </Switch>
   );
 };
