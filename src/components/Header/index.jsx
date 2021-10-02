4a45bb85f727e4620051423e9966a9d2797df141
Ivo Pereira <ivopereira.jr12@gmail.com>
2021-10-02 18:33:42 -0300

    :ok_hand: itens no carrinho e ajustes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,12 @@ import { MdAddShoppingCart } from 'react-icons/md';
 
 import '../../styles/assistants.scss';
 import './styles.scss';
+import { useCart } from '../../hooks/useCart.jsx';
 
 export default function Header() {
+  const { cart } = useCart();
+  const cartSize = cart.length;
+
   return (
     <header className="container">
       <nav className="header-content">
@@ -15,7 +19,7 @@ export default function Header() {
         <Link to="cart" className="cart">
           <div>
             <strong>Meu carrinho</strong>
-            <span>0 item</span>
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </div>
           <MdAddShoppingCart size={36} color="#333" />
         </Link>
